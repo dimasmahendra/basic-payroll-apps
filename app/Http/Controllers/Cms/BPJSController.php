53ca5c5cc2420e5e342b669b7c7eb4869c2266e0
Dimas Mahendra Kusuma <dimasmahendrakusuma@gmail.com>
2023-05-12 16:45:59 +0700

    Update

--- app/Http/Controllers/Cms/BPJSController.php
+++ app/Http/Controllers/Cms/BPJSController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Cms;
 
+use App\Models\Karyawan;
+use App\Models\KomponenKaryawan;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -40,8 +42,52 @@ class BPJSController extends Controller
                 }
             }
             DB::commit();
+
+            $this->updateKaryawan($request->all());
+
             return redirect(route('bpjs'))->with("message", "Berhasil Simpan");
-        } catch (\Throwable $th) {
+        } catch (Throwable $th) {
+            DB::rollBack();
+            dd($th->getMessage());
+        }
+    }
+
+    public function updateKaryawan($data)
+    {
+        try {
+
+            DB::beginTransaction();
+
+            ini_set("memory_limit", "-1");
+
+            $find = Karyawan::get();
+            $chunks = $find->chunk(15);
+            foreach ($chunks as $k => $v) {
+                
+                foreach ($v as $i => $data_karyawan) {
+                    $model = KomponenKaryawan::whereIn('komponen_nama', ['bpjs_kesehatan', 'bpjs_tenagakerja', 'bpjs_orangtua'])
+                                ->where('karyawan_id', $data_karyawan->id)
+                                ->get();
+
+                    foreach ($model as $e => $kk) {
+                        if ($data_karyawan->bpjs_kesehatan == 1 && $kk->komponen_nama == 'bpjs_kesehatan') {
+                            $kk->komponen_nilai = $data['bpjs_kesehatan'];
+                        }
+                        if ($data_karyawan->bpjs_tenagakerja == 1 && $kk->komponen_nama == 'bpjs_tenagakerja') {
+                            $kk->komponen_nilai = $data['bpjs_tenagakerja'];
+                        }
+                        if ($data_karyawan->bpjs_orangtua == 1 && $kk->komponen_nama == 'bpjs_orangtua') {
+                            $kk->komponen_nilai = $data['bpjs_orangtua'] * $data_karyawan->jumlah_orangtua;
+                        }
+                        $kk->save();
+                    }
+                }
+            }
+
+            DB::commit();
+
+            return true;
+        } catch (Throwable $th) {
             DB::rollBack();
             dd($th->getMessage());
         }
